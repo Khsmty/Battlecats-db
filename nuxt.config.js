f81b7b7a434f037f1fc32e8c31c9bc91a0a7610a
Tubuanha (つぶあん派) <70151015+tubuanha@users.noreply.github.com>
2021-10-24 22:05:48 +0900

    fix: ビルドエラー

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: [],
     theme: {
       dark: true,
       themes: {
