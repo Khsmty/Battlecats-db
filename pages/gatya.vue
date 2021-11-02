aac3ed989e1517af18a9c20a3500b5a20bb72a96
Taiga Saito <taiga_saito@yahoo.ne.jp>
2021-11-02 17:35:17 +0900

    ガチャAPI作成

---
+++ pages/gatya.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>Hello World!</p>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  data() {
+    return {
+      message: 'ガチャスケジュール',
+    }
+  },
+  mounted() {
+    this.getGatya()
+  },
+  methods: {
+    async getGatya() {
+      const response = await Axios.get('https://battlecats-api.f5.si/eventlist')
+      const events = response.data
+
+      alert(events)
+    },
+  },
+}
+</script>
