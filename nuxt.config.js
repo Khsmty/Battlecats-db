951c420cb28adf5e5c6c648ebc930fe928ba3e2c
Taiga Saito <taiga_saito@yahoo.ne.jp>
2021-11-17 17:14:58 +0900

    change: static テスト

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - にゃんこ大戦争db',
