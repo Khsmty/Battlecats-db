0f0cdf42fba47c7820aa8f3d23f4bc84f3c352ac
Taiga Saito <taiga_saito@yahoo.ne.jp>
2021-11-18 00:43:13 +0900

    fix: エラーとスタイル

--- api/index.js
+++ api/index.js
@@ -5,6 +5,16 @@ const unitDetail = require('./routes/unitDetail.js')
 const unitList = require('./routes/unitList.js')
 const upcomingEvent = require('./routes/upcomingEvent.js')
 
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 app.get('/api', (_req, res) => {
   res.status(301).redirect('/')
 })
